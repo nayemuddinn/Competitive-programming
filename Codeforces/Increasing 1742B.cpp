1164a1abd449773bf2528251991c81a9b450c416
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-05-31 22:00:18 +0600

    Add files via upload

---
+++ Codeforces/Increasing 1742B.cpp
@@ -0,0 +1,64 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+
+        ll f=0;
+
+        ll a[n];
+
+        For(i,n)
+        cin>>a[i];
+
+
+        sort(a,a+n);
+
+        For(i,n-1)
+        {
+            if(a[i]==a[i+1])
+            {
+                f=1;
+                break;
+            }
+        }
+
+
+        if(f==1)
+            cout<<"NO"<<ndl;
+        else
+            cout<<"YES"<<ndl;
+
+
+    }
+
+
+
+}
+
