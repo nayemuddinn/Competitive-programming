898c2d0da6e7a8672f863052acc0fe34c53ee040
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-06-16 11:22:20 +0600

    Add files via upload

---
+++ Codeforces/Arithmetic Array 1537A.cpp
@@ -0,0 +1,50 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n;
+    cin>>t;
+
+    while(t--)
+    {
+        ll sum=0;
+        cin>>n;
+
+        ll a[n];
+
+        For(i,n)
+        {
+            cin>>a[i];
+            sum+=a[i];
+        }
+
+        if(sum==n) cout<<0<<ndl;
+        else if (sum<n) cout<<1<<ndl;
+        else    cout<<sum-n<<ndl;
+
+
+    }
+
+}
+
