4cb163e59df490ee049d1ed2be48259f5e5fc900
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-06-27 16:59:14 +0600

    Add files via upload

---
+++ Codeforces/System of Equations 214A.cpp
@@ -0,0 +1,45 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+   first_in_out
+
+
+   ll n,m,b,c=0;
+
+   cin>>n>>m;
+
+
+    For3(a,0,sqrt(n))
+    {
+         b=n-a*a;
+
+         if(a+(b*b)==m)
+            c++;
+    }
+
+    cout<<c<<ndl;
+
+
+
+}
+
