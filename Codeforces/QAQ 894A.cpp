10e925d23f2be84cccb3e35cbf923be5566a04e8
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-06-27 16:56:19 +0600

    Add files via upload

---
+++ Codeforces/QAQ 894A.cpp
@@ -0,0 +1,49 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    string s;
+
+    cin>>s;
+
+    ll ans=0;
+
+    For(i,s.length())
+    {
+        for(ll j=i+1; j<s.length(); j++)
+        {
+            for(ll k=j+1; k<s.length(); k++)
+            {
+                if(s[i]=='Q' and s[j]=='A'  and s[k]=='Q')
+                    ans++;
+            }
+        }
+    }
+
+    cout<<ans;
+
+
+
+}
+
