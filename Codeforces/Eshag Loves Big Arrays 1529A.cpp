ba767cf36603168c9b784cc69589e3c67c84a0f4
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-01 18:14:58 +0600

    Add files via upload

---
+++ Codeforces/Eshag Loves Big Arrays 1529A.cpp
@@ -0,0 +1,54 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n,x;
+    cin>>t;
+
+    map<ll,ll>m;
+
+    while(t--)
+    {
+        cin>>n;
+
+        ll minn=1000;
+        m.clear();
+
+        For(i,n)
+        {
+            cin>>x;
+            m[x]++;
+            minn=min(minn,x);
+
+        }
+
+        if(m[minn]==n)
+            cout<<0<<ndl;
+        else
+            cout<<n-m[minn]<<ndl;
+    }
+
+
+
+}
