47c8bf37263fa91c0a7b768b93b9c29f7b8f0ebb
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-07 18:41:27 +0600

    Add files via upload

---
+++ Codeforces/Equalize Prices Again  1234A.cpp
@@ -0,0 +1,49 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+   first_in_out
+
+   ll t,n,x,sum;
+   cin>>t;
+
+   while(t--)
+   {
+      cin>>n;
+      sum=0;
+
+      For(i,n)
+      {
+          cin>>x;
+          sum+=x;
+      }
+
+      if(sum%n==0)
+        cout<<sum/n<<ndl;
+      else
+        cout<<(sum/n)+1<<ndl;
+   }
+
+
+
+}
+
