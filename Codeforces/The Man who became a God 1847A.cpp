8f3499f6925bd60a4b6e62fd27304745ada7c2aa
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-13 11:02:51 +0600

    Add files via upload

---
+++ Codeforces/The Man who became a God 1847A.cpp
@@ -0,0 +1,57 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n,k;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>k;
+
+        ll a[n],b[n-1],sum=0;
+
+        For(i,n)
+        cin>>a[i];
+
+        For(i,n-1)
+        {
+
+            b[i]=abs(a[i]-a[i+1]);
+             sum+=b[i];
+        }
+
+        sort(b,b+n-1,greater<int>());
+
+        For(i,k-1)
+        sum-=b[i];
+
+        cout<<sum<<ndl;
+
+    }
+
+
+
+}
+
