f925e37b70ecb55f6807fe7893459f639a9cd63a
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-14 21:29:24 +0600

    Add files via upload

---
+++ Codeforces/Find and Replace 1807C.cpp
@@ -0,0 +1,79 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n,f;
+    cin>>t;
+
+    string s;
+
+    char a,bit;
+
+    while(t--)
+    {
+        cin>>n>>s;
+
+        f=0;
+
+        For(i,s.size())
+        {
+            a=s[i];
+
+            if(i%2==0)
+                bit='0';
+            else
+                bit='1';
+
+            For(i,n)
+            {
+                if(s[i]==a)
+                    s[i]=bit;
+            }
+
+
+        }
+
+
+
+
+        For(i,s.size()-1)
+        {
+            if(s[i]==s[i+1])
+            {
+                f=1;
+                break;
+            }
+        }
+
+
+        if(f==0)
+            cout<<"YES"<<ndl;
+        else
+            cout<<"NO"<<ndl;
+    }
+
+
+
+}
+
