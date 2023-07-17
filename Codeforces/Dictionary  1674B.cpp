0c434d10040131b4be69d66030479e872a5a8336
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-17 16:45:50 +0600

    Add files via upload

---
+++ Codeforces/Dictionary  1674B.cpp
@@ -0,0 +1,55 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,index,first;
+
+    string n;
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+
+
+        index=(n[0]-97)*25;
+        if(n[1]>n[0])
+            index+=n[1]-97;
+        else
+            index+=n[1]-96;
+
+
+
+        cout<<index<<ndl;
+
+
+
+
+    }
+
+
+
+}
+
