9f0b69b23f2f1943398185814de25e09c6ae95f6
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-20 10:58:46 +0600

    Add files via upload

---
+++ Codeforces/Petya and Origami 1080A.cpp
@@ -0,0 +1,41 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll n,k,a,b,c;
+
+
+    cin>>n>>k;
+
+    a=ceil((double)(n*2)/k);
+    b=ceil((double)(n*5)/k);
+    c=ceil((double)(n*8)/k);
+
+    cout<<a+b+c<<ndl;
+
+
+
+
+}
+
