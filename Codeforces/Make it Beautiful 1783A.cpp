daf3eb246e4ea0adedb3bfe587b8558e278f227e
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-20 11:38:42 +0600

    Add files via upload

---
+++ Codeforces/Make it Beautiful 1783A.cpp
@@ -0,0 +1,66 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n;
+    cin>>t;
+
+    map<ll,ll>m;
+
+    while(t--)
+    {
+        cin>>n;
+
+        m.clear();
+        ll a[n];
+
+        For(i,n)
+        {
+            cin>>a[i];
+            m[a[i]]++;
+        }
+
+        sort(a,a+n);
+
+
+
+        if(m.size()==1)
+            cout<<"NO"<<ndl;
+        else
+        {
+            cout<<"YES"<<ndl;
+            cout<<a[n-1]<<" ";
+
+            For(i,n-1)
+            cout<<a[i]<<" ";
+            cout<<ndl;
+
+        }
+
+    }
+
+
+
+}
+
