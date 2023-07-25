c2188b1dc9513f28601e9d1687ae0fe939e04975
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-26 00:04:26 +0600

    Add files via upload

---
+++ Codeforces/Escalator Conversations.cpp
@@ -0,0 +1,52 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n,m,k,H,x;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>m>>k>>H;
+
+
+        ll c=0;
+
+        For(i,n)
+        {
+            cin>>x;
+
+            if(abs((H-x))%k==0  and  abs((H-x))<=(m-1)*k and abs((H-x))>0)
+                c++;
+
+        }
+
+        cout<<c<<ndl;
+
+
+    }
+
+
+
+}
