da005101017415a8ba5eddbec976977c7c718413
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-07-26 22:23:02 +0600

    Add files via upload

---
+++ Codeforces/Teams Forming 1092B.cpp
@@ -0,0 +1,43 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+
+    ll n,ans=0;
+    cin>>n;
+
+    ll a[n];
+
+    For(i,n)
+    cin>>a[i];
+
+    sort(a,a+n);
+
+    for(ll i=1; i<n; i+=2)
+        ans+=a[i]-a[i-1];
+
+    cout<<ans<<ndl;
+
+}
+
