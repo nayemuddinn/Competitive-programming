babbe3296067019ac44cd97a7bf70f333d0c17ff
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-08-01 02:01:20 +0600

    Add files via upload

---
+++ Codeforces/Nearest Interesting Number 1183A.cpp
@@ -0,0 +1,63 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+
+int main()
+{
+    first_in_out
+
+    ll n,sum=0,len;
+    cin>>n;
+    len=n;
+
+    while(len!=0)
+    {
+
+        sum+=len%10;
+        len=len/10;
+    }
+
+    while(sum%4!=0)
+    {
+        n++;
+        len=n;
+        sum=0;
+
+        while(len!=0)
+        {
+
+            sum+=len%10;
+            len=len/10;
+        }
+
+    }
+
+    cout<<n<<ndl;
+
+
+
+
+
+}
+
+
+
+
