313c4fa0ed29501a8787c33a8c02b78be478581b
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-08-02 22:34:41 +0600

    Add files via upload

---
+++ Codeforces/Tit for Tat 1516A.cpp
@@ -0,0 +1,63 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll t,n,k;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>k;
+
+        ll a[n];
+
+        For(i,n)
+        cin>>a[i];
+
+        ll i=0; ll j=n-1;
+
+        while(k!=0  and i<j)
+        {
+            if(a[i]!=0)
+            {
+                a[i]--;
+                a[j]++;
+                k--;
+            }
+            else
+                i++;
+
+        }
+
+        For(i,n)
+        cout<<a[i]<<" ";
+        cout<<ndl;
+
+
+    }
+
+
+
+}
+
