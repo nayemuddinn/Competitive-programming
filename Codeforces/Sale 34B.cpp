c6a8556ec18ecc28b9f903612eb0016f4373c05d
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-08-09 09:57:37 +0600

    Add files via upload

---
+++ Codeforces/Sale 34B.cpp
@@ -0,0 +1,52 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll n,m,sum=0;
+
+    cin>>n>>m;
+
+    vector<ll>v(n);
+
+    For(i,n)
+    cin>>v[i];
+
+   sortV(v);
+
+
+    For(i,m)
+    {
+      if(v[i]<0)
+        sum+=v[i];
+      else
+        break;
+    }
+
+    cout<<abs(sum);
+
+
+
+
+}
+
