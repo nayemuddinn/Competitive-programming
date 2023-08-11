288e6112d8a0411f837e0d8a1d111605d15afbc8
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-08-11 12:02:38 +0600

    Add files via upload

---
+++ Codeforces/Construct the String 1335B.cpp
@@ -0,0 +1,50 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+
+    ll t,n,a,b;
+    cin>>t;
+
+    string s="qwertyuiopasdfghjklzxcvbnm";
+    while(t--)
+    {
+        cin>>n>>a>>b;
+
+        ll i=0;
+        For(j,n)
+        {
+
+            cout<<s[i];
+            i++;
+
+            if(i>=b)
+                i=0;
+        }
+        cout<<ndl;
+
+
+    }
+
+}
