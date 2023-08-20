cbcc96aac69889def850b4de9654a811b69bc104
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-08-20 22:48:37 +0600

    Add files via upload

---
+++ Codeforces/Lunch Rush 276A.cpp
@@ -0,0 +1,48 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    ll n,f,k,t;
+    cin>>n>>k;
+
+
+    map<ll,ll>m;
+
+    while(n--)
+    {
+        cin>>f>>t;
+
+        if(t>k)
+            m.insert({f-(t-k),0});
+        else
+            m.insert({f,0});
+
+    }
+
+    cout<<(--m.end())->first;
+
+
+
+}
+
