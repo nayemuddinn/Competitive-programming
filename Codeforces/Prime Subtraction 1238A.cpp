b4e3c45c161252dea2c7410d59b698e841eda43b
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-09-05 05:41:24 +0600

    Add files via upload

---
+++ Codeforces/Prime Subtraction 1238A.cpp
@@ -0,0 +1,43 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+   first_in_out
+
+   ll t,x,y;
+   cin>>t;
+
+   while(t--)
+   {
+      cin>>x>>y;
+
+      if(x-y>1)
+        cout<<"YES";
+      else
+        cout<<"NO";
+      cout<<ndl;
+   }
+
+
+
+}
+
