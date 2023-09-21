bffa057a28529c3e3ebdc39448801dc250802cab
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-09-21 23:26:11 +0600

    Add files via upload

---
+++ Codeforces/1D Eraser.cpp
@@ -0,0 +1,53 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define For4(i,n)          for(int i=n-1; i>=0; i--)
+#define ndl                "\n"
+#define ll                 long long int
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define sortV(v)           sort(v.begin(),v.end())
+
+
+int main()
+{
+    first_in_out
+
+    string s;
+
+    ll t,n,k;
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>k>>s;
+
+        ll c=0;
+
+        For(i,n)
+        {
+            if(s[i]=='B')
+            {
+                i+=k-1;
+                c++;
+            }
+
+        }
+
+        cout<<c<<ndl;
+    }
+
+
+
+}
