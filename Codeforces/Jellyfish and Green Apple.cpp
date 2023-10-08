3461373b986788fcfdafb2f37068ff0f4c5a26dc
Nayem Uddin <126597905+nayemuddinn@users.noreply.github.com>
2023-10-08 22:05:52 +0600

    Add files via upload

---
+++ Codeforces/Jellyfish and Green Apple.cpp
@@ -0,0 +1,102 @@
+/**
+.
+  author->  NATE
+.
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define first_in_out       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define For(i,n)           for(int i=0;i<n;i++)
+#define For2(i,n)          for(int i=1;i<=n;i++)
+#define For3(i, j, n)      for (int i = j; i <= n; i++)
+#define For4(i,n)          for(int i=n-1; i>=0; i--)
+#define ndl                "\n"
+#define ll                 long long int
+#define double             long double
+#define PI                 3.1415926535897932384626
+#define INF                1e18
+#define pb                 push_back
+#define F                  first
+#define S                  second
+#define max_heap           priority_queue <ll>
+#define min_heap           priority_queue <ll, vector<ll>, greater<ll>>
+#define sortAll(v)         sort(v.begin(),v.end())
+#define bug(...)           __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)           for(auto x : a) cout << x << " "; cout << endl
+#define printpair(a)       for(auto x : a) cout << x.F << " " << x.S << endl
+
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1)
+{
+	cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | ";
+	__f (comma + 1, args...);
+}
+
+void solve()
+{
+	ll n, m, rem, c = 0, i = 1;
+	cin >> n >> m;
+
+	rem = n % m;
+	unordered_map<ll, ll> map;
+	if (n >= m and rem == 0)
+		c = 0;
+	else if ( m & 1)
+		c = -1;
+	else
+	{
+
+
+		while (rem % m != 0)
+		{
+
+			c += rem;
+			rem *= 2;
+			map[rem]++;
+			if (map[rem] == 2 or i > 50)
+			{
+				c = -1;
+				break;
+			}
+
+			if (rem > m)
+				rem = rem - m;
+
+			i++;
+
+
+		}
+
+	}
+	cout << c << ndl;
+}
+
+
+
+
+int main()
+{
+	first_in_out
+
+	//clock_t z = clock();
+
+	ll t = 1;
+	cin >> t;
+
+	while (t--)
+		solve();
+
+	//cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+
+}
